a7d9bb7f3caa2c2f113817b8324673e9457ff843
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-07-06 08:29:42 -0500

    styled components playing nicely with ssr

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,49 @@
-import { AppProps } from "next/app";
 import { ApolloProvider } from "@apollo/react-hooks";
 import { useApollo } from "../lib/apollo";
+import React from "react";
+import Head from "next/head";
+import { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
+import { Footer, Header } from "../components";
+import { Center } from "../components/Style";
+import { GlobalStyle, theme } from "../global";
+import "antd/dist/antd.css";
+
+export function reportWebVitals(metric: any) {
+	console.log(metric)
+}
 
 export default function App({ Component, pageProps }: AppProps) {
 	const apolloClient = useApollo(pageProps.initialApolloState);
-
 	return (
 		<ApolloProvider client={apolloClient}>
-			<Component {...pageProps} />
+		<ThemeProvider theme={theme}>
+			<GlobalStyle theme={theme} />
+			<Head>
+				
+			</Head>
+			<Head>
+				<title>Next.ts</title>
+			</Head>
+			<Header />
+			<main className="main">
+				<Center>
+					<Component {...pageProps} />
+				</Center>
+			</main>
+			<Footer />
+		</ThemeProvider>
 		</ApolloProvider>
-	);
+	)
 }
+
+
+// export default function App({ Component, pageProps }: AppProps) {
+// 	const apolloClient = useApollo(pageProps.initialApolloState);
+
+// 	return (
+// 		<ApolloProvider client={apolloClient}>
+// 			<Component {...pageProps} />
+// 		</ApolloProvider>
+// 	);
+// }
