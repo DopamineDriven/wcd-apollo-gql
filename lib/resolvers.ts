c2226ba432f43d4de63495dccb1ce34f9ea9428a
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-07-12 11:55:24 -0500

    successfully mapped queried data

--- lib/resolvers.ts
+++ lib/resolvers.ts
@@ -2,9 +2,17 @@ import { QueryResolvers } from "./type-defs.graphqls";
 import { ResolverContext } from "./apollo";
 import users from "./userData";
 import { User } from "./viewer.graphql";
+
+interface UserArgs {
+	id: string;
+}
+
 const Query: Required<QueryResolvers<ResolverContext>> = {
-	viewers(_parent, args, _context, _info): User[] {
-		users.find((user) => user.id === args);
+	viewers(_parent, { args }: UserArgs | any, _context, _info): User[] {
+		users.find((user) => {
+			user.id === args;
+		});
+		console.log(users)
 		return users;
 	}
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,94 +4,8 @@ import Link from "next/link";
 import { useViewerQuery, ViewerDocument } from "../lib/viewer.graphql";
 import { initializeApollo } from "../lib/apollo";
 import { User } from "../lib/viewer.graphql";
-import styled from "styled-components";
-// import apolloServer from './api/graphql';
+import { Card } from "antd";
 
-export const CardPostCard = styled.a`
-	border-radius: 6px;
-	overflow: hidden;
-	background-color: #fff;
-	box-shadow: 0 0 0 1px rgba(0, 0, 0, 0.035), 0 4px 25px rgba(0, 0, 0, 0.07);
-	color: black;
-	text-decoration: none;
-	transition: all 0.2s;
-
-	&:hover {
-		color: black;
-		box-shadow: 0 0 0 1px rgba(0, 0, 0, 0.035), 0 6px 35px rgba(0, 0, 0, 0.2);
-		transform: translateY(-2px);
-	}
-`;
-
-export const FigurePostCard = styled.figure`
-	padding: 56% 0 0;
-	margin: 0;
-	vertical-align: center;
-	max-width: 100%;
-	position: relative;
-	overflow: hidden;
-	border-radius: 6px 6px 0 0;
-
-	img {
-		max-width: 100%;
-		position: absolute;
-		top: 0;
-		left: 0;
-	}
-`;
-
-export const TitlePostCard = styled.h3`
-	margin: 10px 20px;
-	font-size: 1.4rem;
-`;
-
-export const LeadPostCard = styled.div`
-	margin: 0 20px 20px;
-
-	& > * {
-		margin: 0 0 10px;
-	}
-`;
-
-export const GridSection = styled.div`
-	display: flex;
-	flex-wrap: wrap;
-	justify-content: space-between;
-
-	&:after {
-		content: "";
-		flex: auto;
-	}
-
-	&:after,
-	& > * {
-		width: calc(30% - 2px);
-		margin-bottom: 20px;
-	}
-
-	@media (max-width: 800px) {
-		&:after,
-		& > * {
-			width: 100%;
-		}
-	}
-`;
-
-export const TitleSection = styled.h2`
-	font-size: 2.8rem;
-	line-height: 1.1;
-	margin: 10px 0 15px;
-
-	@media (max-width: 800px) {
-		font-size: 2rem;
-	}
-`;
-
-export const LinkSection = styled.a`
-	margin: -20px 0 30px;
-	display: inline-block;
-	vertical-align: top;
-`;
 
 interface Props {
 	user: User;
@@ -101,56 +15,39 @@ interface IndexProps {
 	users: User[];
 }
 
-const PostCard: FC<Props> = ({ user }) => {
-	return (
-		<CardPostCard>
-			<FigurePostCard>
-				<img alt={user.name} src={user.image} />
-			</FigurePostCard>
-			<TitlePostCard>{user.name}</TitlePostCard>
-			<LeadPostCard>{user.role}</LeadPostCard>
-		</CardPostCard>
-	);
+const { Meta } = Card;
+
+const gridStyle = {
+	width: "33.33%",
+	textAlign: "justify"
 };
 
-const Section: FC<IndexProps> = ({ users }) => {
-	return (
-		<section>
-			<GridSection>
-				{users.map((user) => {
-					<PostCard key={user.id} user={user} />;
-				})}
-			</GridSection>
-			<Link href="/about">
-				<a>about</a>
-			</Link>{" "}
-		</section>
-	);
-};
+// const PostCard: FC<Props> = ({ user }) => {
+// 	return (
+// 		<CardPostCard>
+// 			<FigurePostCard>
+// 				<img alt={user.name} src={user.image} />
+// 			</FigurePostCard>
+// 			<TitlePostCard>{user.name}</TitlePostCard>
+// 			<LeadPostCard>{user.role}</LeadPostCard>
+// 		</CardPostCard>
+// 	);
+// };
 
-const Index: NextPage = () => {
-	const { data, loading, error } = useViewerQuery();
-	if (data) {
-		const { viewers } = data!;
-
-		return error ? (
-			<div>oops! error</div>
-		) : loading ? (
-			<div>loading...</div>
-		) : (
-			<div>
-
-				<Section users={viewers} />
-				go to the{" "}
-				<Link href="/about">
-					<a>about</a>
-				</Link>{" "}
-				page.
-			</div>
-		);
-	}
-	return <div></div>;
-};
+// const Section: FC<IndexProps> = ({ users }) => {
+// 	return (
+// 		<section>
+// 			<GridSection>
+// 				{users.map((user) => {
+// 					<PostCard key={user.id} user={user} />;
+// 				})}
+// 			</GridSection>
+// 			<Link href="/about">
+// 				<a>about</a>
+// 			</Link>{" "}
+// 		</section>
+// 	);
+// };
 
 export async function getStaticProps({}: GetStaticProps) {
 	const apolloClient = initializeApollo();
@@ -165,5 +62,36 @@ export async function getStaticProps({}: GetStaticProps) {
 		}
 	};
 }
+
+const Index: NextPage = () => {
+	const { data, loading, error } = useViewerQuery();
+	if (data) {
+		const { viewers } = data!;
+		console.log(viewers);
+
+		const items = viewers.map((viewer) => (
+			<Card.Grid key={viewer.id} hoverable={true} style={gridStyle}>
+				{viewer.name}—{viewer.role}
+				<br />
+			</Card.Grid>
+		));
+
+		return error ? (
+			<div>oops! error</div>
+		) : loading ? (
+			<div>loading...</div>
+		) : (
+			<div>
+				<ul>{items}</ul>
+				go to the{" "}
+				<Link href="/about">
+					<a>about</a>
+				</Link>{" "}
+				page.
+			</div>
+		);
+	}
+	return <div></div>;
+};
 
 export default Index;
