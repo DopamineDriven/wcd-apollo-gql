01e436211eb4bc86479680f325d713436bebebf2
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-07-13 02:43:35 -0500

    building out a custom layout/navbar

---
+++ components/FixedHeader/index.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react";
+import { Layout, Menu, Breadcrumb } from "antd";
+const { Header, Content, Footer } = Layout;
+
+const FixedHeader: FC = () => {
+	return (
+		<Layout>
+			<Header className="header-fixed">
+
+            </Header>
+		</Layout>
+	);
+};
