92941bb72f98fa618e630e94725d0351ea423983
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-07-15 02:56:27 -0500

    updates

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,47 +1,70 @@
-import React, { FC } from "react";
-import { NextPage } from "next";
+import React from "react";
+import Head from "next/head";
 import Link from "next/link";
-import styled from "styled-components";
 
-const ContainerSOS = styled.div`
-	display: flex;
-	flex-wrap: wrap;
-	justify-content: center;
-	align-items: center;
-	text-align: center;
-`;
-
-const MainSOS = styled.h2`
-	font-size: 10rem;
-	line-height: 11rem;
-	font-family: ${(p) => p.theme.fonts.accent};
-	width: 100%;
-`;
-
-const SubSOS = styled.h3`
-	font-size: 5rem;
-	line-height: 11rem;
-	font-family: ${(p) => p.theme.fonts.accent};
-	width: 100%;
-`;
-
-const LinkSOS = styled.a`
-	font-size: 3.5rem;
-	line-height: 4rem;
-	font-family: ${(p) => p.theme.fonts.accent};
-	width: 100%;
-`;
-
-const NotFound: FC & NextPage = () => {
+export default function Custom404() {
+	const title = "Return Home";
+	const statusCode = 404;
 	return (
-		<ContainerSOS>
-			<MainSOS>404</MainSOS>
-			<SubSOS>That&apos;s an error</SubSOS>
-			<Link href="/">
-				<LinkSOS>&rarr;&nbsp;Home&nbsp;&larr;</LinkSOS>
-			</Link>
-		</ContainerSOS>
+		<div style={styles.error}>
+			<Head>
+				<title>
+					{statusCode}: {title}
+				</title>
+			</Head>
+			<div>
+				<style dangerouslySetInnerHTML={{ __html: "body { margin: 0 }" }} />
+				{statusCode ? <h1 style={styles.h1}>{statusCode}</h1> : null}
+				<div style={styles.desc}>
+					<Link href="/">
+						<a style={styles.a}>&rarr;&nbsp;{title}&nbsp;&larr;</a>
+					</Link>
+				</div>
+			</div>
+		</div>
 	);
-};
+}
 
-export default NotFound;
+const styles: { [k: string]: React.CSSProperties } = {
+	error: {
+		color: "#000",
+		background: "#fff",
+		fontFamily:
+			'-apple-system, BlinkMacSystemFont, Roboto, "Segoe UI", "Fira Sans", Avenir, "Helvetica Neue", "Lucida Grande", sans-serif',
+		height: "100vh",
+		textAlign: "center",
+		display: "flex",
+		flexDirection: "column",
+		alignItems: "center",
+		justifyContent: "center"
+	},
+
+	desc: {
+		display: "inline-block",
+		textAlign: "left",
+		lineHeight: "100px",
+		height: "100px",
+		verticalAlign: "middle"
+	},
+
+	h1: {
+		display: "inline-block",
+		borderRight: "1px solid rgba(0, 0, 0,.3)",
+		margin: 0,
+		marginRight: "20px",
+		padding: "10px 23px 10px 0",
+		fontSize: "24px",
+		fontWeight: 500,
+		verticalAlign: "middle"
+	},
+
+	a: {
+		fontSize: "14px",
+		fontWeight: "normal",
+		lineHeight: "inherit",
+		margin: 0,
+		padding: 0,
+		color: "blue",
+		verticalAlign: "top"
+	}
+};
