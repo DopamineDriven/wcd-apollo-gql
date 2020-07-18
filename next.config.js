e32a76ebe23ee31fb04760358d876f86c7928234
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-07-18 00:44:49 -0500

    optimizing the fuck out of this app

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+const gql = {
 	webpack(config, options) {
 		config.module.rules.push({
 			test: /\.graphql$/,
@@ -15,6 +15,55 @@ module.exports = {
 		return config;
 	}
 };
+
+const compose = plugins => ({
+	webpack(config, options) {
+		return plugins.reduce((config, plugin) => {
+			if (plugin instanceof Array) {
+				const [_plugin, ...args] = plugin;
+				plugin = _plugin(...args);
+			}
+			if (plugin instanceof Function) {
+				plugin = plugin();
+			}
+			if (plugin && plugin.webpack instanceof Function) {
+				return plugin.webpack(config, options);
+			}
+			return config;
+		}, config);
+	},
+
+	webpackDevMiddleware(config) {
+		return plugins.reduce((config, plugin) => {
+			if (plugin instanceof Array) {
+				const [_plugin, ...args] = plugin;
+				plugin = _plugin(...args);
+			}
+			if (plugin instanceof Function) {
+				plugin = plugin();
+			}
+			if (plugin && plugin.webpackDevMiddleware instanceof Function) {
+				return plugin.webpackDevMiddleware(config);
+			}
+			return config;
+		}, config);
+	}
+});
+
+const withBundleAnalyzer = require("@next/bundle-analyzer");
+
+module.exports = compose([
+	[
+
+		withBundleAnalyzer,
+		{
+			enabled: process.env.ANALYZE === "true"
+		}
+	],
+	gql
+]);
+
+
 
 // const withPlugins = require("next-compose-plugins");
 // const withBundleAnalyzer = require("@next/bundle-analyzer");
