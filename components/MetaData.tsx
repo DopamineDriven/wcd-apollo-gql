262e60302195dcbf54a8fabd80a65b4e938ea15f
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-07-19 13:00:32 -0500

    updates

--- components/MetaData.tsx
+++ components/MetaData.tsx
@@ -64,6 +64,24 @@ const MetaData = () => {
 				sizes="192x192"
 				href="/favicon/apple-icon-precomposed.png"
 			/>
+			<link
+				rel="icon"
+				type="image/png"
+				sizes="96x96"
+				href="/favicon/favicon-96x96.png"
+			/>
+			<link
+				rel="icon"
+				type="image/png"
+				sizes="32x32"
+				href="/favicon/favicon-32x32.png"
+			/>
+			<link
+				rel="icon"
+				type="image/png"
+				sizes="16x16"
+				href="/favicon/favicon-16x16.png"
+			/>
 			<link rel="manifest" href="/favicon/site.webmanifest" />
 			<link
 				rel="mask-icon"
